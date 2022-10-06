01fbaeaae1c4638147175ef856defb3b37e4d090
liu <2294397905@qq.com>
2022-10-06 14:34:05 +0800

    自定义转换器

--- meiduo_mall/meiduo_mall/urls.py
+++ meiduo_mall/meiduo_mall/urls.py
@@ -15,6 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from utils.converters import UsernameConverter
+from django.urls import register_converter
+
+register_converter(UsernameConverter, 'username')
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('apps.users.urls')),
