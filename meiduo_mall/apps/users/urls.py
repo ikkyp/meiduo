a3276cbabdded0da667dd5135cbdacfddf695d44
liu <2294397905@qq.com>
2022-10-10 21:11:40 +0800

    celery异步发送邮件以及邮件的激活

--- meiduo_mall/apps/users/urls.py
+++ meiduo_mall/apps/users/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from apps.users.views import UsernameCountView, RegisterView, LoginView, LogoutView, CenterView
-
+from apps.users.views import UsernameCountView, RegisterView, LoginView, LogoutView, CenterView, EmailView
+from apps.users.views import EmailVerifyView
 urlpatterns = [
     path('usernames/<username:username>/count/', UsernameCountView.as_view()),
     path('register/', RegisterView.as_view()),
     path('login/', LoginView.as_view()),
     path('logout/', LogoutView.as_view()),
-    path('info/', CenterView.as_view())
+    path('info/', CenterView.as_view()),
+    path('emails/', EmailView.as_view()),
+    path('emails/verification/', EmailVerifyView.as_view()),
 ]
