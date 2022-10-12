35082dda589c575f53a9e926865d63bd5fde34bf
liu <2294397905@qq.com>
2022-10-12 17:07:57 +0800

    cache缓存的实现

--- meiduo_mall/apps/areas/views.py
+++ meiduo_mall/apps/areas/views.py
@@ -1,3 +1,4 @@
+from django.core.cache import cache
 from django.http import JsonResponse
 from django.views import View
 
@@ -7,26 +8,32 @@ from apps.areas.models import Area
 # Create your views here.
 class AreasView(View):
     def get(self, request):
-        provinces = Area.objects.filter(parent=None)
-        # provinces获取的是对象数据，不能转化为json数据，需要转化为字典数据才行
-        province_list = []
-        for province in provinces:
-            province_list.append({
-                'id': province.id,
-                'name': province.name
-            })
+        province_list = cache.get('province')
+        if province_list is None:
+            provinces = Area.objects.filter(parent=None)
+            # provinces获取的是对象数据，不能转化为json数据，需要转化为字典数据才行
+            province_list = []
+            for province in provinces:
+                province_list.append({
+                    'id': province.id,
+                    'name': province.name
+                })
+            cache.set('province', province_list, 24 * 3600)
         return JsonResponse({'code': 0, 'errmsg': 'ok', 'province_list': province_list})
 
 
 class SubAreasView(View):
     def get(self, request, id):
         # 包揽省市区
-        up_level = Area.objects.get(id=id)
-        down_level = up_level.subs.all()
-        data_list = []
-        for item in down_level:
-            data_list.append({
-                'id': item.id,
-                'name': item.name,
-            })
+        data_list = cache.get('data_list_%s' % id)
+        if data_list is None:
+            up_level = Area.objects.get(id=id)
+            down_level = up_level.subs.all()
+            data_list = []
+            for item in down_level:
+                data_list.append({
+                    'id': item.id,
+                    'name': item.name,
+                })
+            cache.set('data_list_%s' % id, data_list, 24 * 3600)
         return JsonResponse({'code': 0, 'errmsg': 'ok', 'sub_data': {'subs': data_list}})
