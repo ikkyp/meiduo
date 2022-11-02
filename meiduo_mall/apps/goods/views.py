a886f560863f53721ac39e3642ebe9b86ac66e65
liu <2294397905@qq.com>
2022-11-02 21:08:52 +0800

    列表数据的展示

--- meiduo_mall/apps/goods/views.py
+++ meiduo_mall/apps/goods/views.py
@@ -1,7 +1,12 @@
+from django.core.paginator import Paginator
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
 
 from apps.contents.models import ContentCategory
+from apps.goods.models import GoodsCategory
+from apps.goods.models import SKU
+from utils.goods import get_breadcrumb
 from utils.goods import get_categories
 
 
@@ -31,4 +36,50 @@ class IndexView(View):
         return render(request, 'index.html', context)
 
 
+class ListView(View):
 
+    def get(self, request, category_id):
+        # 1.接收参数
+        # 排序字段
+        ordering = request.GET.get('ordering')
+        # 每页多少条数据
+        page_size = request.GET.get('page_size')
+        # 要第几页数据
+        page = request.GET.get('page')
+
+        # 2.获取分类id
+        # 3.根据分类id进行分类数据的查询验证
+        try:
+            category = GoodsCategory.objects.get(id=category_id)
+        except GoodsCategory.DoesNotExist:
+            return JsonResponse({'code': 400, 'errmsg': '参数缺失'})
+        # 4.获取面包屑数据
+        breadcrumb = get_breadcrumb(category)
+
+        # 5.查询分类对应的sku数据，然后排序，然后分页
+        skus = SKU.objects.filter(category=category, is_launched=True).order_by(ordering)
+        # 分页
+
+        # object_list, per_page
+        # object_list   列表数据
+        # per_page      每页多少条数据
+        paginator = Paginator(skus, per_page=page_size)
+
+        # 获取指定页码的数据
+        page_skus = paginator.page(page)
+
+        sku_list = []
+        # 将对象转换为字典数据
+        for sku in page_skus.object_list:
+            sku_list.append({
+                'id': sku.id,
+                'name': sku.name,
+                'price': sku.price,
+                'default_image_url': sku.default_image.url
+            })
+
+        # 获取总页码
+        total_num = paginator.num_pages
+
+        # 6.返回响应
+        return JsonResponse({'code': 0, 'errmsg': 'ok', 'list': sku_list, 'count': total_num, 'breadcrumb': breadcrumb})
